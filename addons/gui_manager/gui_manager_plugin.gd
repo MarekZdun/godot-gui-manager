f80298444d776f646646c8a5f6c5b7f2600fb11b
Marek Zdun <marek.sebastian.zdun@gmail.com>
2026-03-07 17:33:08 +0100

    Rename enable_plugin() to _enable_plugin()

--- addons/gui_manager/gui_manager_plugin.gd
+++ addons/gui_manager/gui_manager_plugin.gd
@@ -5,7 +5,7 @@ const GUI_MANAGER_FILEPATH = "res://addons/gui_manager/gui_manager.tscn"
 const GUI_MANAGER_AUTLOAD_NAME = "GuiManager"
 
 
-func enable_plugin():
+func _enable_plugin():
 	add_autoload_singleton(GUI_MANAGER_AUTLOAD_NAME, GUI_MANAGER_FILEPATH)
 
 
